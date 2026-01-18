c4c75767f20bbb186e0c5aee9165847099ccef44
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2026-01-18 07:10:18 +0200

    bouncer writes jsonl, reflect this in the name

--- bouncer.bat
+++ bouncer.bat
@@ -1,3 +1,3 @@
-@for /F %%x in ('"luvit -e print(os.date('^%%Y^%%m^%%d^%%H^%%M^%%S',os.time()))"') do set OUT=wss-%%x.txt
+@for /F %%x in ('"luvit -e print(os.date('^%%Y^%%m^%%d^%%H^%%M^%%S',os.time()))"') do set OUT=wss-%%x.jsonl
 echo %OUT%>lastout.txt
 ncat -lk 54197 | websocat %WS% | tee -a %OUT%
