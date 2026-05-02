8f2506d11d1db9543190568f97a32c903d457625
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2026-05-02 15:25:12 +0300

    Don't dump the token to console in chat and wss either

--- chat.bat
+++ chat.bat
@@ -1,6 +1,6 @@
 call setup
-set URL=https://%HOST%/api/chat?accessToken=%TOKEN%
+@set URL=https://%HOST%/api/chat?accessToken=%TOKEN%
 set OUT=chat-%T%.json
 echo %OUT%>data\chat-last.txt
 
-curl %URL% > data\%OUT%
+@curl %URL% > data\%OUT%
