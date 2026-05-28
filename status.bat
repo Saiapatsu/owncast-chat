12454cd8813f289ae9237408f9dd48a0560171eb
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2026-05-29 02:22:20 +0300

    status: bump interval to 300s since it's unimportant and unused

--- status.bat
+++ status.bat
@@ -4,6 +4,6 @@ set OUT=status-%T%.jsonl
 echo %OUT%>data\status-last.txt
 
 :loop
-curl --rate 1/60s %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% | tee -a data\%OUT%
-timeout 60
+curl --rate 1/300s %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% %URL% | tee -a data\%OUT%
+timeout 300
 goto loop
